2cc4ece7c0c0ad0e88e05cd4004bb6a8332fa462
restarhalf <1793982252@qq.com>
2025-10-21 18:57:40 +0800

    I

--- build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MusicPlayer_FILE D:/C_C++Package/CLionProject/Qt6_MusicPlayer/build/Release/Debug/MusicPlayer.exe)
-set(__QT_DEPLOY_TARGET_MusicPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MusicPlayer_RUNTIME_DLLS D:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Qt/6.9.1/msvc2022_64/bin/Qt6Multimediad.dll;D:/vcpkg/installed/x64-windows/debug/bin/tag_c.dll;D:/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;D:/Qt/6.9.1/msvc2022_64/bin/Qt6Networkd.dll;D:/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll;D:/vcpkg/installed/x64-windows/debug/bin/tag.dll)
